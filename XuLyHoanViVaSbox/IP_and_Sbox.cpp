4a72859210e0095029e8d37deaf9b0672e1cb045
DungVT2005 <tridungvu2k5@gmail.com>
2026-04-18 03:37:29 +0700

    fix

--- XuLyHoanViVaSbox/IP_and_Sbox.cpp
+++ XuLyHoanViVaSbox/IP_and_Sbox.cpp
@@ -9,23 +9,23 @@ namespace des_sim {
 
 // Hàm để hoán vị bit theo các bảng cho trước
 uint64_t permute_bits(uint64_t bits, const vector<int>& table, int input_size) {
-    uint64_t result = 0;
-    int output_size = static_cast<int>(table.size());
+    uint64_t result = 0; // Kết quả sau hoán vị
+    int output_size = static_cast<int>(table.size()); // Số bit đầu ra
     for (int i = 0; i < output_size; ++i) {
         int bit_pos = table[i];
         // Lấy bit từ vị trí giá trị của table[i], lấy giá trị của bit đó
         uint64_t bit_val = (bits >> (input_size - bit_pos)) & 1ULL;
-        result |= bit_val << (output_size - 1 - i);
+        result |= bit_val << (output_size - 1 - i);// Đưa bit  vào vị trí 
     }
     return result;
 }
 
 // Hàm để thực hiện hoán vị IP và FP
 uint64_t initial_permutation(uint64_t block_64) {
-    return permute_bits(block_64, vector<int>(IP.begin(), IP.end()), 64);
+    return permute_bits(block_64, vector<int>(IP.begin(), IP.end()), 64); //  IP
 }
 uint64_t final_permutation(uint64_t block_64) {
-    return permute_bits(block_64, vector<int>(FP.begin(), FP.end()), 64);
+    return permute_bits(block_64, vector<int>(FP.begin(), FP.end()), 64);// 
 }
 
 // Hàm thực hiện một vòng Feistel, trả về cặp (L', R') sau khi áp dụng hàm F và hoán vị
@@ -33,7 +33,7 @@ pair<uint32_t, uint32_t> feistel_round(
     uint32_t L, uint32_t R, uint64_t subkey, const function<uint32_t(uint32_t, uint64_t)>& f_fn, int round_num, bool verbose
 ) {
     if (verbose) cout << "Round " << round_num << " L=" << hex << L << " R=" << R << " K=" << subkey << "\n";
-    uint32_t f_result = f_fn(R, subkey);
+    uint32_t f_result = f_fn(R, subkey);//  F(R, K)
     return {R, static_cast<uint32_t>(L ^ f_result)};
 }
 
@@ -48,10 +48,9 @@ int sbox_lookup(int sbox_index, uint8_t six_bits) {
 uint32_t feistel_f(uint32_t R, uint64_t subkey) {
     //Mở rộng R từ bảng E
     uint64_t r_expanded = permute_bits(R, E_TABLE, 32);
-    uint64_t xor_result = r_expanded ^ subkey;
-    // Cho qua S_box
-    uint32_t s_output = sbox_substitute(xor_result);
-    return static_cast<uint32_t>(permute_bits(s_output, vector<int>(P_TABLE.begin(), P_TABLE.end()), 32));
+    uint64_t xor_result = r_expanded ^ subkey; // XOR subkey
+    uint32_t s_output = sbox_substitute(xor_result);// Cho qua S_box
+    return static_cast<uint32_t>(permute_bits(s_output, vector<int>(P_TABLE.begin(), P_TABLE.end()), 32));// Hoán vị P 
     //Hoán vị kết quả qua bảng P và trả về
 }
 // Hàm thay thế 48 bit qua S-box thành 32 bit
@@ -60,10 +59,10 @@ uint32_t sbox_substitute(uint64_t bits_48) {
     for (int i = 0; i < 8; ++i) {
         int shift = (7 - i) * 6;
         uint8_t six_bits = static_cast<uint8_t>((bits_48 >> shift) & 0x3F);
-        int four_bits = sbox_lookup(i, six_bits);
+        int four_bits = sbox_lookup(i, six_bits); // Tra S-box
         result |= static_cast<uint32_t>(four_bits) << ((7 - i) * 4);
     }
     return result;
 }
 
-}  // namespace des_sim
+}  
