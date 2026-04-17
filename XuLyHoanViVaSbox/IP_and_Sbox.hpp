759c9a223cf54df484244e7b0e4e471d8b9b0ebc
DungVT2005 <tridungvu2k5@gmail.com>
2026-04-18 03:40:10 +0700

    fix

--- XuLyHoanViVaSbox/IP_and_Sbox.hpp
+++ XuLyHoanViVaSbox/IP_and_Sbox.hpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 namespace des_sim {
-uint64_t permute_bits(uint64_t bits, const vector<int>& table, int input_size);
-uint64_t initial_permutation(uint64_t block_64);
-uint64_t final_permutation(uint64_t block_64);
-pair<uint32_t, uint32_t> feistel_round(
+uint64_t permute_bits(uint64_t bits, const vector<int>& table, int input_size); // Dữ liệu đầu vào,Bảng hoán vị,Số bit đầu vào
+uint64_t initial_permutation(uint64_t block_64); //  IP 
+uint64_t final_permutation(uint64_t block_64);//  FP
+pair<uint32_t, uint32_t> feistel_round( // 1 VÒNG FEISTEL
     uint32_t L, uint32_t R, uint64_t subkey, const function<uint32_t(uint32_t, uint64_t)>& f_fn, int round_num = 0, bool verbose = false
 );
-int sbox_lookup(int sbox_index, uint8_t six_bits);
+int sbox_lookup(int sbox_index, uint8_t six_bits);  // TRA S-BOX
 uint32_t sbox_substitute(uint64_t bits_48);
 uint32_t feistel_f(uint32_t R, uint64_t subkey);
 }
